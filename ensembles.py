8c749bd96530e68f99bb80ad71d8bf61b28b60da
Alexey-Borisov <72136589+Alexey-Borisov@users.noreply.github.com>
2021-12-20 16:03:25 +0300

    Add files via upload
    Add logging to ensembles. final version ensembles

--- ensembles.py
+++ ensembles.py
@@ -1,10 +1,11 @@
 import numpy as np
+import time
 from scipy.optimize import minimize_scalar
 from sklearn.tree import DecisionTreeRegressor
 
 
-def mse(y_true, y_pred):
-    return ((y_true - y_pred) ** 2).mean()
+def rmse(y_true, y_pred):
+    return np.sqrt(((y_true - y_pred) ** 2).mean())
 
 
 class RandomForestMSE:
@@ -29,6 +30,7 @@ class RandomForestMSE:
             self.feature_subsample_size = 1 / 3
         self.trees_parameters = trees_parameters
         self.ensemble = []
+        self.history = None
 
     def fit(self, X, y, X_val=None, y_val=None):
         """
@@ -44,7 +46,8 @@ class RandomForestMSE:
         y_val : numpy ndarray
             Array of size n_val_objects
         """
-
+        self.history = {'time': [], 'pred_loss': []}
+        start_time = time.time()
         for i in range(self.n_estimators):
             model = DecisionTreeRegressor(max_depth=self.max_depth, **self.trees_parameters)
             feature_subsample = np.random.choice(X.shape[1], size=int(X.shape[1] * self.feature_subsample_size),
@@ -53,13 +56,17 @@ class RandomForestMSE:
             model.fit(X[object_subsample][:, feature_subsample], y[object_subsample])
             self.ensemble.append((model, feature_subsample))
 
+            # time logging
+            cur_time = time.time()
+            self.history['time'].append(cur_time - start_time) 
+
         # validation
         if X_val is not None and y_val is not None:
-            y_val_pred = self.predict(X_val)
-            val_score = mse(y_val, y_val_pred)
+            y_val_pred = self.predict(X_val, None, y_val)
+            val_score = rmse(y_val, y_val_pred)
             return val_score
 
-    def predict(self, X, n_estimators=None):
+    def predict(self, X, n_estimators=None, y_true=None):
         """
         X : numpy ndarray
             Array of size n_objects, n_features
@@ -72,8 +79,12 @@ class RandomForestMSE:
         if n_estimators is None or n_estimators > self.n_estimators:
             n_estimators = self.n_estimators
         y_pred = np.zeros(X.shape[0])
-        for model, feature_subsample in self.ensemble[:n_estimators]:
+        for cur_idx, (model, feature_subsample) in enumerate(self.ensemble[:n_estimators]):
             y_pred += model.predict(X[:, feature_subsample])
+
+            if y_true is not None:
+                loss = rmse(y_true, y_pred / (cur_idx + 1))
+                self.history['pred_loss'].append(loss)
         return y_pred / n_estimators
     
     
@@ -103,6 +114,7 @@ class GradientBoostingMSE:
         self.max_depth = max_depth
         self.trees_parameters = trees_parameters
         self.ensemble = []
+        self.history = None
 
     def fit(self, X, y, X_val=None, y_val=None):
         """
@@ -112,28 +124,36 @@ class GradientBoostingMSE:
         y : numpy ndarray
             Array of size n_objects
         """
+        
         # loss function for one-dimensional optimization
         def loss(alpha):
             return ((f + alpha * model_pred - y)**2).mean()
 
-        f = np.zeros_like(y)
+        f = np.zeros_like(y, dtype=float)
+        self.history = {'time': [], 'pred_loss': []}
+        start_time = time.time()
         for i in range(self.n_estimators):
             model = DecisionTreeRegressor(max_depth=self.max_depth, **self.trees_parameters)
             feature_subsample = np.random.choice(X.shape[1], size=int(X.shape[1] * self.feature_subsample_size),
                                                  replace=False)
             model.fit(X[:, feature_subsample], (y - f))
             model_pred = model.predict(X[:, feature_subsample])
+
             optim_alpha = minimize_scalar(loss).x
             f += optim_alpha * self.learning_rate * model_pred
             self.ensemble.append((model, feature_subsample, optim_alpha * self.learning_rate))
+            
+            # time logging
+            cur_time = time.time()
+            self.history['time'].append(cur_time - start_time) 
 
         # validation
         if X_val is not None and y_val is not None:
-            y_val_pred = self.predict(X_val)
-            val_score = mse(y_val, y_val_pred)
+            y_val_pred = self.predict(X_val, None, y_val)
+            val_score = rmse(y_val, y_val_pred)
             return val_score
 
-    def predict(self, X):
+    def predict(self, X, n_estimators=None, y_true=None):
         """
         X : numpy ndarray
             Array of size n_objects, n_features
@@ -143,7 +163,13 @@ class GradientBoostingMSE:
         y : numpy ndarray
             Array of size n_objects
         """
+        if n_estimators is None or n_estimators > self.n_estimators:
+            n_estimators = self.n_estimators
         y_pred = np.zeros(X.shape[0])
-        for model, feature_subsample, coef in self.ensemble:
+        for model, feature_subsample, coef in self.ensemble[:n_estimators]:
             y_pred += coef * model.predict(X[:, feature_subsample])
+
+            if y_true is not None:
+                loss = rmse(y_true, y_pred)
+                self.history['pred_loss'].append(loss)
         return y_pred
